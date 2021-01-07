51595a8eb88a9c1f505a53eb0ffc1d8a554b65d6
Rostikys <77107900+Rostikys@users.noreply.github.com>
2021-01-07 15:21:27 +0200

    first commit
    first

---
+++ HomeWork1/Experement.rb
@@ -0,0 +1,49 @@
+
+def funk
+  {test:4,
+   test2:5}
+end
+
+
+funk.each do |k,v|
+  puts k
+end
+
+
+
+
+
+
+
+#{pereminaInt = 24
+#puts (pereminaInt)
+
+#pereminaString = "Peremina String"
+#puts (pereminaString)                 урок 2
+
+#pereminaDouble = 9.2
+#puts (pereminaDouble)
+
+#perebinaTrueOrFalse = true
+#puts (perebinaTrueOrFalse)
+#---
+
+#zmString = "shos "
+#zmOklString= "!"
+#puts (zmString+zmOklString)
+#puts (zmString.length)
+#puts (zmString.include? "shos")
+
+#zm = -25.34
+#puts (zm.abs())
+#puts (zm.round())
+#zm = zm.abs()
+#zm = zm.round()
+#puts (Math.sqrt(zm))
+#---
+
+
+
+
+
+
